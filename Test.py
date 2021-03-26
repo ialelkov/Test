921d74df83842be218a525b6c0731a614ed5f516
ialelkov <81433974+ialelkov@users.noreply.github.com>
2021-03-26 19:34:06 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,19 @@
+import pyppeteer
+import time
+
+import asyncio
+from pyppeteer import launch
+
+async def main():
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+    await page.goto('https://www.rbc.ru/')
+    n = int(input("Введите количество скриншотов "))
+    t = int(input("Введите задержку "))
+    for i in range(n):
+        s = "rbk" + str(i) + ".jpg"
+        await page.screenshot({'path': s})
+        time.sleep(t)
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
